eff64ccd67d7576c4a794709dbb0b243f09a88f2
KatherineStrider <kotopesska112@yandex.ru>
2017-04-26 19:02:43 +0400

    Создание главного активити, заполнение его контентом

--- app/src/main/java/com/example/kate/beautystyle/MainActivity.java
+++ app/src/main/java/com/example/kate/beautystyle/MainActivity.java
@@ -2,20 +2,38 @@ package com.example.kate.beautystyle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        recyclerView = (RecyclerView) findViewById(R.id.recycleView);
 
         Picasso.with(this)
-                .load("https://pp.userapi.com/c637524/v637524796/2b378/ZgvTpZVtf30.jpg")
+                .load("http://www.dandy-dogs.ru/template/images/banner2.png")
                 .into(imageView);
+
+        RecycleViewAdapter recyclerViewAdapter = new RecycleViewAdapter(this, getResources().getStringArray(R.array.services),onNameClickListener);
+
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    RecycleViewAdapter.OnNameClickListener onNameClickListener = new RecycleViewAdapter.OnNameClickListener() {
+        @Override
+        public void onNameClick(int position, String name) {
+            Toast.makeText(MainActivity.this, String.format("%s #%d", name, position), Toast.LENGTH_SHORT).show();
+        }
+    };
 }
--- app/src/main/java/com/example/kate/beautystyle/RecycleViewAdapter.java
+++ app/src/main/java/com/example/kate/beautystyle/RecycleViewAdapter.java
@@ -1,8 +1,96 @@
 package com.example.kate.beautystyle;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by Kate on 26.04.2017.
  */
 
-public class RecycleViewAdapter {
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.ContactViewHolder> {
+
+    private LayoutInflater inflater;
+    private String[] strings;
+    private OnNameClickListener clickListener;
+    private Context context;
+
+    public RecycleViewAdapter(Context context, String[] strings, OnNameClickListener clickListener){
+        this.inflater = LayoutInflater.from(context);
+        this.context = context;
+        this.strings = strings;
+        this.clickListener = clickListener;
+    }
+
+    @Override
+    public int getItemCount() {
+        return strings == null ? 0 : strings.length;
+    }
+
+    @Override
+    public RecycleViewAdapter.ContactViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.view_recycle, parent, false);
+        final ContactViewHolder contactViewHolder = new ContactViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = contactViewHolder.getAdapterPosition();
+                if (position == RecyclerView.NO_POSITION) return;
+                clickListener.onNameClick(position, strings[position]);
+            }
+        });
+        return contactViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecycleViewAdapter.ContactViewHolder holder, int position) {
+        holder.bind(context, strings[position]);
+    }
+
+    static class ContactViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageTypeService;
+        TextView textTypeService;
+
+        ContactViewHolder(View itemView){
+            super(itemView);
+
+            imageTypeService = (ImageView) itemView.findViewById(R.id.imageService);
+            textTypeService = (TextView) itemView.findViewById(R.id.textService);
+        }
+
+        void bind(Context context, String value){
+
+            String url = "";
+
+            switch (value){
+                case "стрижка":
+                    url = "https://pp.userapi.com/c405124/v405124786/532d/mU3dY12rZhM.jpg";
+                    break;
+                case "покраска":
+                    url = "http://n1s2.hsmedia.ru/41/6b/73/416b7303e40fb2ce0a4ba26786f33c63/600x322_1_c095cd8a5d2cff6e98ac8691784f1db4@600x322_0x59f91261_17955022721411752153.jpeg";
+                    break;
+                case "маникюр":
+                    url = "http://dogcity.com.ua/wp-content/uploads/2017/01/IMG_9697.jpg";
+                    break;
+                default:
+                    break;
+            }
+
+            Picasso.with(context)
+                    .load(url)
+                    .into(imageTypeService);
+            textTypeService.setText(value);
+        }
+    }
+
+    public interface OnNameClickListener {
+        void onNameClick(int position, String name);
+    }
 }
