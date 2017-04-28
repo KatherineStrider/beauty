b1b91ff722ee9b78c079197d49c301d0341a344b
KatherineStrider <kotopesska112@yandex.ru>
2017-04-28 18:04:50 +0400

    Попытки изменить это приложение к лучшему (закомментированы)

--- app/src/main/java/com/example/kate/beautystyle/ServicesActivity.java
+++ app/src/main/java/com/example/kate/beautystyle/ServicesActivity.java
@@ -3,6 +3,7 @@ package com.example.kate.beautystyle;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -15,11 +16,13 @@ import com.example.kate.beautystyle.fragments.ServicesInfo;
 
 public class ServicesActivity extends AppCompatActivity implements OnServiceClickListener {
 
+    Fragment fragment;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
         Log.d("supertag", "Мы в Services");
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.frame, ServicesInfo.getInstance(), ServicesInfo.TAG)
@@ -29,13 +32,13 @@ public class ServicesActivity extends AppCompatActivity implements OnServiceClic
 
     @Override
     public void onServiceClick() {
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.frame_for_detail, DetailInfo.getInstance(), DetailInfo.TAG)
                     .addToBackStack(DetailInfo.TAG)
                     .commit();
-        }else {
+        } else {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.frame, DetailInfo.getInstance(), DetailInfo.TAG)
@@ -49,8 +52,24 @@ public class ServicesActivity extends AppCompatActivity implements OnServiceClic
         if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
             getSupportFragmentManager().popBackStackImmediate();
         }
+//        else if(getSupportFragmentManager().getBackStackEntryCount() < 1
+//                || getSupportFragmentManager().getBackStackEntryCount() == 2 && getResources()
+//                .getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+//            this.finish();
+//        }
         else {
             super.onBackPressed();
+            this.finish();
         }
     }
+
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//    }
+//
+//    @Override
+//    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//    }
 }
